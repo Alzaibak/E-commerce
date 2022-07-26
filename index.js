4968203fa852ca8b60d3595617e4526cb05a50f4
alzaibak <alzaibak.abdalaziz@gmail.com>
2022-07-26 20:44:20 +0200

    user routes adding

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+// express, mongodb server connection 
 const express = require("express");
 const app = express();
 const moongose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require ("./routes/user")
 
 dotenv.config();
 
 moongose
 .connect(process.env.MONGO_URL)
 .then (()=> console.log("DB connection Done")).catch((err)=>console.log("error"));
-app.listen(5000,() =>{
+
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000,() =>{
     console.log("connected")
 })
