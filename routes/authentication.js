c70367e4e22a5087be35e5dd56e491d18acd3ab2
alzaibak <alzaibak.abdalaziz@gmail.com>
2022-08-03 22:18:34 +0200

    Token verification code

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,8 @@
+// User login and register file 
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
 const user = require("../models/USER");
+const JsonWebToken = require("jsonwebtoken");
 
 // New user registration
 router.post("/Register", async (req, res) =>{
@@ -19,19 +21,26 @@ router.post("/Register", async (req, res) =>{
     }
 });
 
-// user Login
-
+// user Login (user request like typing email or username (req) server respone (res))
 router.post("/login", async (req, res)=>{
     try {
+        // 1. searching user in database
         const userInfo = await user.findOne({email: req.body.email});
-        
         !userInfo && res.status(401).json("Wrong email");
-
+        // 2. decrypting user password
         const decrptPAssword = CryptoJS.AES.decrypt(userInfo.password, process.env.SECRET_PASSWORD);
-        const password = decrptPAssword.toString(CryptoJS.enc.Utf8);
-        password !== req.body.password && res.status(401).json("Wrong password");
-        res.status(200).json(userInfo);
-
+        const userPassword = decrptPAssword.toString(CryptoJS.enc.Utf8);
+        userPassword !== req.body.password && res.status(401).json("Wrong password");
+        
+        // 3. User id verification using JWT
+        const verifingToken = JsonWebToken.sign(
+            {
+                id: user._id,
+                isAdmine: user.isAdmine,
+            },process.env.JWT_SECRET_KEY,{expiresIn:"3d"}
+        );
+        // 4. Sending user information
+        res.status(200).json({userInfo, verifingToken});
     } catch (error) {
         res.status(500).json("Error");
     }
